acbf15b14bf390b63669abd1843e7c21b8c15e54
elohorpromise2 <elohorpromise2@gmail.com>
2026-09-08 09:13:20 +0100

    Complete W02 DOM events activity

--- scripts/getdates.js
+++
@@ -1,6 +0,0 @@
-// WDD131 getdates.js - dynamic footer dates
-// Copyright year
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-// Last modified - using document.lastModified string (no formatting needed per assignment)
-document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
